6747d3d1cec7d1ebc84b50de7726e0204930cd9a
EllieRaine04 <elanande@ucsc.edu>
2024-05-24 13:24:55 -0700

    homepage update

--- js/site.js
+++ js/site.js
@@ -2,3 +2,22 @@
 // Author: Elisa Anderson 
 // Date:5-14-24
 
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollContainer = document.getElementById("album-scroll");
+    const scrollLeftButton = document.getElementById("scroll-left");
+    const scrollRightButton = document.getElementById("scroll-right");
+
+    scrollLeftButton.addEventListener("click", function() {
+        scrollContainer.scrollBy({
+            left: -150, // Adjust this value to scroll more or less
+            behavior: 'smooth'
+        });
+    });
+
+    scrollRightButton.addEventListener("click", function() {
+        scrollContainer.scrollBy({
+            left: 150, // Adjust this value to scroll more or less
+            behavior: 'smooth'
+        });
+    });
+});
--- page1/js/lab.js
+++ page1/js/lab.js
@@ -9,22 +9,3 @@ Rory Gershen-Lewis, Joyce Cha, Freddie Torres
 //of their name
 
 // script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const scrollContainer = document.getElementById("album-scroll");
-    const scrollLeftButton = document.getElementById("scroll-left");
-    const scrollRightButton = document.getElementById("scroll-right");
-
-    scrollLeftButton.addEventListener("click", function() {
-        scrollContainer.scrollBy({
-            left: -150, // Adjust this value to scroll more or less
-            behavior: 'smooth'
-        });
-    });
-
-    scrollRightButton.addEventListener("click", function() {
-        scrollContainer.scrollBy({
-            left: 150, // Adjust this value to scroll more or less
-            behavior: 'smooth'
-        });
-    });
-});
